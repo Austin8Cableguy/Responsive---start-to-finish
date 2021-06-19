ab0b7ee80f47f64f397b8ad6ca1694e023bbf269
Austin8Cableguy <Austin8Cableguy@outlook.com>
2021-06-19 15:32:18 -0500

    updated to add more pages

--- README.md
+++
@@ -1,4 +0,0 @@
-# Responsive---start-to-finish
-
-Kevin Powell's tutorial How to Create a Website - Complete workflow
-https://www.youtube.com/watch?v=aEUkm1as3KE
--- js/main.js
+++ js/main.js
@@ -1,10 +1,43 @@
-$(document).ready(function() {
+const about = document.querySelector('#about')
+const contact = document.querySelector('#contact')
+const aboutContent = document.querySelector('#about-content')
+const contactContent = document.querySelector('#contact-content')
 
-    // hamburger menu toggle
-    $('.nav-toggle').click(function() {
-      $('.main-nav').toggleClass('is-open');
-      $('.hamburger').toggleClass('is-open');
-    })
-  
+about.addEventListener('click', () => {
+  const aboutBox = new WinBox({
+    title: 'Time Zones',
+    // modal: true,
+    width: '960px',
+    height: '600px',
+    top: 50,
+    right: 50,
+    bottom: 50,
+    left: 50,
+    mount: aboutContent,
+    onfocus: function () {
+      this.setBackground('#00aa00')
+    },
+    onblur: function () {
+      this.setBackground('#777')
+    },
   })
-  
+})
+
+contact.addEventListener('click', () => {
+  const contactBox = new WinBox({
+    title: 'Contact Me',
+    width: '400px',
+    height: '400px',
+    top: 150,
+    right: 50,
+    bottom: 50,
+    left: 250,
+    mount: contactContent,
+    onfocus: function () {
+      this.setBackground('#00aa00')
+    },
+    onblur: function () {
+      this.setBackground('#777')
+    },
+  })
+})
